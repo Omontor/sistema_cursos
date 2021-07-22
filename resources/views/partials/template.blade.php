fc39242d6465e80eb04df9d7fcab98e9f7c22d72
Omontor <omontor@gmail.com>
2021-07-22 12:51:39 -0500

    template implementada
    ya sirven todos los includes.
    las nuevas vistas se deben crear usando extends a partials.template  (ver ejemplo en welcome.blade

---
+++ resources/views/partials/template.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    @yield('header')
+    @include('partials.header')
+</head>
+<body>
+    @include('partials.loader')
+    <div class="wrap-header">
+        @include('partials.nav')
+    </div><!-- wrap-header -->
+    @yield('content')
+    @include('partials.footer')
+    @yield('scripts')
+    @include('partials.scripts')
+</body>
+</html>
